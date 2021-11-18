e94377ecc2d280c1801e77cddf54f7949a9a221e
amulligan <alanmugz@gmail.com>
2021-11-18 19:41:06 +0000

    F0000 - Updated circleci config

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app vvvvvvvvvvv!</Text>
+      <Text>Gav, your a knob!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
